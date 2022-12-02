2b17a7857f056f628e06af278d2109d8caa94952
Nuri Abidin <nuri.abidin@gmail.com>
2022-12-02 23:33:55 +0700

    Add files via upload
    iOS Static Table Project

---
+++ MySimpleApp/MySimpleApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MySimpleApp
+//
+//  Created by nuri on 02/12/22.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
